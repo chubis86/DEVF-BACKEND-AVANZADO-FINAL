767b0b74fab711a8a6ce4a26ea6b1ae969c353f7
EFREN NARVAEZ <efrennm86@gmail.com>
2024-01-19 11:00:58 -0600

    Se instaló el express async handler que sirve para funciones asíncronas en lugar del try-catch y se modificó el archivo de los controladores de usuario para implementar esta herramienta.

--- backend/controllers/usuariosController.js
+++ backend/controllers/usuariosController.js
@@ -1,10 +1,20 @@
-const login = (req, res) => {
-    res.status(200).json({message:'Logueo'})
- }
+const asyncHandler = require(express-async-hanldler)
 
-const registrarUsuario = (req, res) => {
-    res.status(201).json({message:'Registrar Usuario'})
- }
+const login = asyncHandler ( async (req, res) => {
+    if(!req.body.email || !req.body.password){
+        res.status(400)
+        throw new Error ("Hace falta email y/o password")
+    }
+    res.status(200).json({message:'Logueo'})
+ })
+
+ const registrarUsuario = asyncHandler ( async (req, res) => {
+    if(!req.body.email || !req.body.password || !req.body.nombre){
+        res.status(400)
+        throw new Error ("Hacen faltan valores")
+    }
+    res.status(200).json({message:'Usuario registrado'})
+})
 
 
 module.exports = {
